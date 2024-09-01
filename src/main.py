b34cffeccfc9a313f3de8881a3ca098e0eb934f2
Kyle Fu <43867185+Giantpizzahead@users.noreply.github.com>
2024-08-31 20:03:39 -0700

    force memory limit on linux

--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
 """Main runner for the bot."""
 
+import resource
+
 import bobbot
+
+logger = bobbot.get_logger(__name__)
 
 
 def main() -> None:
     """Run the bot."""
+    try:
+        limit_mb = 512  # Heroku 512 MB RAM limit
+        limit_bytes = limit_mb * 1024 * 1024
+        resource.setrlimit(resource.RLIMIT_AS, (limit_bytes, limit_bytes))
+        logger.info(f"Memory limit set to {limit_mb} MB.")
+    except Exception:
+        pass
     bobbot.run_bot()
-    bobbot.get_logger(__name__).info("Stopping...")
+    logger.info("Stopping...")
 
 
 if __name__ == "__main__":
