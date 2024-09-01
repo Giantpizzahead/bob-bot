1dbfdd6a72a9c4e143996867b2e41459bdc66540
Kyle Fu <43867185+Giantpizzahead@users.noreply.github.com>
2024-08-31 20:59:22 -0700

    raised mem limit again

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 """Main runner for the bot."""
 
+import os
 import resource
 
 import bobbot
@@ -10,7 +11,9 @@ logger = bobbot.get_logger(__name__)
 def main() -> None:
     """Run the bot."""
     try:
-        limit_mb = 960  # Heroku 960 MB RAM limit
+        limit_mb = 1024 + 128  # Heroku RAM limit
+        if os.getenv("MEMORY_LIMIT"):
+            limit_mb = int(os.getenv("MEMORY_LIMIT"))
         limit_bytes = limit_mb * 1024 * 1024
         resource.setrlimit(resource.RLIMIT_AS, (limit_bytes, limit_bytes))
         logger.info(f"Memory limit set to {limit_mb} MB.")
